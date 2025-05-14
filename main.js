4907ba4806dc67943e10c7852e8c30dd1ace6f3d
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-05-14 12:21:21 -0600

    vite three fps

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -13,16 +14,26 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setAnimationLoop(animate)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
 camera.position.z = 5
 
-function animate() {
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+const loader = new GLTFLoader()
 
+loader.load(
+  "./soldier.glb",
+  function (gltf) {
+    scene.add(gltf.scene)
+  },
+  undefined,
+  function (error) {
+    console.error(error)
+  }
+)
+
+const color = 0xffffff
+const intensity = 1
+const light = new THREE.AmbientLight(color, intensity)
+scene.add(light)
+
+function animate() {
   renderer.render(scene, camera)
 }
